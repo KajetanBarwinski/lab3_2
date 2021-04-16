cf28a3c356fd969b716ea0f548b26184dcbd8f75
Jakub Wlaźlak <222611@edu.p.lodz.pl>
2021-04-16 17:31:10 +0200

    Dodanie testu sprawdzajacego czy po przekroczeniu 24 godzin próba "confirm order" zakończy się rzuceniem wyjątku OrderExpiredException

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -1,19 +1,40 @@
 package edu.iis.mto.time;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 
+@ExtendWith(MockitoExtension.class)
 class OrderTest {
 
+    @Mock
+    private Clock clock;
+    private Order order;
+
     @BeforeEach
-    void setUp() throws Exception {}
+    void setUp() throws Exception {
+        when(clock.getZone()).thenReturn(ZoneId.systemDefault());
+        order = new Order(clock);
+    }
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void checkIfOrderConfirmationAfterTwentyFourHourThrowsOrderExpiredExceptions() {
+        Instant submission = Instant.now();
+        Instant confirmation = submission.plus(26, ChronoUnit.HOURS);
+        when(clock.instant()).thenReturn(submission).thenReturn(confirmation);
+        order.submit();
+        assertThrows(OrderExpiredException.class,() -> order.confirm());
+        assertEquals(order.getOrderState(), Order.State.CANCELLED);
     }
 
 }
