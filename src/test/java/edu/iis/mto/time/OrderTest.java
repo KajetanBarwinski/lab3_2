ddec10ee513584c4150703bd659c554382300d25
Dominik Snopek <222572@edu.p.lodz.pl>
2021-04-16 17:52:43 +0200

    Test expiredOrderStatusSubmited - sprawdza czy zostaje ustawiony status zamowieniana SUBMITTED

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -79,6 +79,36 @@ class OrderTest {
 
     }
 
+    @Test
+    void expiredOrderStatusConfirmedWithLowerValidPeriodHours(){
+        Instant startTime = Instant.parse("2015-12-08T10:00:00Z");
 
+        Order order = new Order(clock);
+        Instant endTime = startTime.plus(Order.VALID_PERIOD_HOURS -1, ChronoUnit.HOURS);
+        when(clock.getZone()).thenReturn(ZoneId.systemDefault());
+        when(clock.instant()).thenReturn(startTime).thenReturn(endTime);
+
+        order.submit();
+        order.confirm();
+
+        assertEquals(order.getOrderState(), Order.State.CONFIRMED);
+
+    }
+
+    @Test
+    void expiredOrderStatusSubmited(){
+        Instant startTime = Instant.parse("2015-12-08T10:00:00Z");
+
+        Order order = new Order(clock);
+        Instant endTime = startTime.plus(Order.VALID_PERIOD_HOURS -1, ChronoUnit.HOURS);
+        when(clock.getZone()).thenReturn(ZoneId.systemDefault());
+        when(clock.instant()).thenReturn(startTime).thenReturn(endTime);
+
+        order.submit();
+
+
+        assertEquals(order.getOrderState(), Order.State.SUBMITTED);
+
+    }
 
 }
