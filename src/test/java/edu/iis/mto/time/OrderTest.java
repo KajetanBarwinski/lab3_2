98e95d18a8cde21ca68fadd43c63f7c1e5ccd937
sublimacja <zuzanna.adamiec@gmail.com>
2021-04-16 17:54:21 +0200

    Dodanie testu testShouldSetConfirmedStatusWhenOrderIsNotExpired

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -24,6 +24,7 @@ class OrderTest {
     private Instant t1;
     private Order order;
     private final long VALUE_GREATER_THAN_VALID_PERIOD_HOUR=25;
+    private final long VALUE_LESS_THAN_VALID_PERIOD_HOUR=24;
     @BeforeEach
     void setUp()  {
         t1=Instant.now();
@@ -37,6 +38,16 @@ class OrderTest {
         order.submit();
         assertThrows(OrderExpiredException.class, () -> order.confirm());
         assertThat(order.getOrderState(), Is.is(Order.State.CANCELLED));
+    }
+
+    @Test
+    void testShouldSetConfirmedStatusWhenOrderIsNotExpired()
+    {
+        when(clock.instant()).thenReturn(t1).thenReturn(t1.plus(VALUE_LESS_THAN_VALID_PERIOD_HOUR, ChronoUnit.HOURS));
+        when(clock.getZone()).thenReturn(ZoneId.systemDefault());
+        order.submit();
+        order.confirm();
+        assertThat(order.getOrderState(), Is.is(Order.State.CONFIRMED));
     }
 
 
