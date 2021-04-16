bd4e6243ac64b0b51b563ceeaa82b0ad9090909e
EDGExD <222585@edu.p.lodz.pl>
2021-04-16 18:31:00 +0200

    Zmodyfikowanie konstruktora Order.

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -1,11 +1,21 @@
 package edu.iis.mto.time;
 
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.ZoneId;
+import org.junit.jupiter.api.Assertions;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-
+@ExtendWith(MockitoExtension.class)
 class OrderTest {
 
     @BeforeEach
@@ -13,7 +23,19 @@ class OrderTest {
 
     @Test
     void test() {
-        fail("Not yet implemented");
+        Clock clock = Clock.fixed(Instant.parse("2030-12-22T10:15:30.00Z"), ZoneId.of("UTC"));
+ 
+
+        LocalDateTime dateTime = LocalDateTime.now(clock);
+
+    
+        Order order = new Order();
+        order.submit();
+        
+        LocalDateTime expected = LocalDateTime.of(2030, Month.DECEMBER, 22, 10, 15, 30);
+
+        
+        Assertions.assertTrue((dateTime).equals(expected));
     }
 
 }
