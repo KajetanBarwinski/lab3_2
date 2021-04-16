ec431ca32fc88d64517e40d8ac0da09d8c58bef0
221003 <221003@edu.p.lodz.pl>
2021-04-16 18:21:51 +0200

    Dodanie testu dla przypadku kiedy czas zamówienia został przekroczony.

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -2,18 +2,43 @@ package edu.iis.mto.time;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class OrderTest {
 
+    @Mock
+    Clock clock;
+	
+    private Order order;
+    private Instant submittionTime;
+    
     @BeforeEach
-    void setUp() throws Exception {}
-
-    @Test
-    void test() {
-        fail("Not yet implemented");
+    void setUp() {
+    	order = new Order(clock);
+    	when(clock.getZone()).thenReturn(ZoneId.systemDefault());
+    	submittionTime = Instant.now();
     }
 
+    @Test
+    void confirmShouldThrowOrderExpiredExceptionAndOrderStateShouldEqualCancelled() {
+    	Instant expiredTime = submittionTime.plus(25, ChronoUnit.HOURS);
+        when(clock.instant()).thenReturn(submittionTime).thenReturn(expiredTime);
+        
+        order.submit();
+        
+        assertThrows(OrderExpiredException.class, order::confirm);
+        assertEquals(Order.State.CANCELLED, order.getOrderState());
+    }
 }
