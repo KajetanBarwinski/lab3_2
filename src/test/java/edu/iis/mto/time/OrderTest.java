60f48280ded1811a8cbdf7d1b4b6982d19dee50b
EDGExD <222585@edu.p.lodz.pl>
2021-04-16 18:58:44 +0200

    Dodanie testów

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -1,41 +1,87 @@
 package edu.iis.mto.time;
 
+import edu.iis.mto.time.Order.State;
 import java.time.Clock;
 import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.Month;
 import java.time.ZoneId;
 import org.junit.jupiter.api.Assertions;
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import static org.mockito.ArgumentMatchers.any;
+import org.mockito.Mock;
 import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class OrderTest {
-
+    @Mock
+    Clock clock;
+    
     @BeforeEach
     void setUp() throws Exception {}
 
     @Test
-    void test() {
-        Clock clock = Clock.fixed(Instant.parse("2030-12-22T10:15:30.00Z"), ZoneId.of("UTC"));
- 
-
-        LocalDateTime dateTime = LocalDateTime.now(clock);
-
-    
-        Order order = new Order();
+    void test_order_canceled_1() {
+        
+        when(clock.getZone()).thenReturn(ZoneId.systemDefault());
+        when(clock.instant()).thenReturn(Instant.parse("2020-01-01T12:00:00Z")).thenReturn(Instant.parse("2020-01-02T13:00:00Z"));
+                
+        Order order = new Order(clock);
         order.submit();
         
-        LocalDateTime expected = LocalDateTime.of(2030, Month.DECEMBER, 22, 10, 15, 30);
-
+        try {
+            order.confirm();
+        }catch (OrderExpiredException e)
+        {
+            Assertions.assertTrue((order.getOrderState()).equals(State.CANCELLED));
+        }
+ 
+        Assertions.assertTrue((order.getOrderState()).equals(State.CANCELLED));
+    }
+    
+    @Test
+    void test_order_canceled_2() {
         
-        Assertions.assertTrue((dateTime).equals(expected));
+        when(clock.getZone()).thenReturn(ZoneId.systemDefault());
+        when(clock.instant()).thenReturn(Instant.parse("2020-01-01T12:00:00Z")).thenReturn(Instant.parse("2021-01-01T12:00:00Z"));
+                
+        Order order = new Order(clock);
+        order.submit();
+        
+        try {
+            order.confirm();
+        }catch (OrderExpiredException e)
+        {
+            Assertions.assertTrue((order.getOrderState()).equals(State.CANCELLED));
+        }
+ 
+        Assertions.assertTrue((order.getOrderState()).equals(State.CANCELLED));
+    }
+    
+    @Test
+    void test_order_not_canceled_1() {
+        
+        when(clock.getZone()).thenReturn(ZoneId.systemDefault());
+        when(clock.instant()).thenReturn(Instant.parse("2020-01-01T12:00:00Z")).thenReturn(Instant.parse("2020-01-02T12:00:00Z"));
+                
+        Order order = new Order(clock);
+        order.submit();              
+        order.confirm();
+        
+        Assertions.assertTrue((order.getOrderState()).equals(State.CONFIRMED));
+    }
+    
+    @Test
+    void test_order_not_canceled_2() {
+        
+        when(clock.getZone()).thenReturn(ZoneId.systemDefault());
+        when(clock.instant()).thenReturn(Instant.parse("2020-01-01T12:00:00Z")).thenReturn(Instant.parse("2020-01-01T12:00:10Z"));
+                
+        Order order = new Order(clock);
+        order.submit();             
+        order.confirm();
+        
+        Assertions.assertTrue((order.getOrderState()).equals(State.CONFIRMED));
     }
 
 }
