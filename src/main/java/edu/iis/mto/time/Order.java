e46385c055f34362ae3664f146c9a46030262d9b
221003 <221003@edu.p.lodz.pl>
2021-04-16 18:14:50 +0200

    Edycja klasy Order

--- src/main/java/edu/iis/mto/time/Order.java
+++ src/main/java/edu/iis/mto/time/Order.java
@@ -1,5 +1,6 @@
 package edu.iis.mto.time;
 
+import java.time.Clock;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
@@ -12,8 +13,16 @@ public class Order {
     private List<OrderItem> items = new ArrayList<>();
     private LocalDateTime subbmitionDate;
 
+    private Clock clock;
+    
     public Order() {
+    	clock = Clock.systemDefaultZone();
         orderState = State.CREATED;
+    }
+    
+    public Order(Clock clock) {
+    	this.clock = clock;
+    	orderState = State.CREATED;
     }
 
     public void addItem(OrderItem item) {
@@ -28,13 +37,13 @@ public class Order {
         requireState(State.CREATED);
 
         orderState = State.SUBMITTED;
-        subbmitionDate = LocalDateTime.now();
+        subbmitionDate = LocalDateTime.now(clock);
 
     }
 
     public void confirm() {
         requireState(State.SUBMITTED);
-        long hoursElapsedAfterSubmittion = subbmitionDate.until(LocalDateTime.now(), ChronoUnit.HOURS);
+        long hoursElapsedAfterSubmittion = subbmitionDate.until(LocalDateTime.now(clock), ChronoUnit.HOURS);
         if (hoursElapsedAfterSubmittion > VALID_PERIOD_HOURS) {
             orderState = State.CANCELLED;
             throw new OrderExpiredException();
