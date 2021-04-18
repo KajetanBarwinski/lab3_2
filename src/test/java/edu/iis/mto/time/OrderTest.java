377ac4e7f10fcc7b8ba241ae11e7f3bb27526d00
Durmix <Durmix@users.noreply.github.com>
2021-04-18 12:11:03 +0200

    Add test cases for exceeded order time

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -1,19 +1,59 @@
 package edu.iis.mto.time;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class OrderTest {
 
+    @Mock
+    private Clock clock;
+
+    private Instant correct;
+    private Order order;
+
     @BeforeEach
-    void setUp() throws Exception {}
+    void setUp() {
+        when(clock.getZone()).thenReturn(ZoneId.systemDefault());
+        order = new Order(clock);
+        correct = Instant.now();
+    }
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void shouldThrowOrderExpiredException_OneHourExceed() {
+        //Given
+        Instant exceed = Instant.now().plus(25, ChronoUnit.HOURS);
+        when(clock.instant()).thenReturn(correct).thenReturn(exceed);
+
+        //When
+        order.submit();
+
+        //Then
+        assertThrows(OrderExpiredException.class, order::confirm);
+    }
+
+    @Test
+    void shouldThrowOrderExpiredException_TwelveHourExceed() {
+        //Given
+        Instant exceed = Instant.now().plus(36, ChronoUnit.HOURS);
+        when(clock.instant()).thenReturn(correct).thenReturn(exceed);
+
+        //When
+        order.submit();
+
+        //Then
+        assertThrows(OrderExpiredException.class, order::confirm);
     }
 
 }
