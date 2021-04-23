11444784c24cbfc0fb7027d812afe2cad8d796e0
kamil <222574@edu.p.lodz.pl>
2021-04-23 15:41:56 +0200

    Added OrderExpirationTest

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -1,19 +1,41 @@
 package edu.iis.mto.time;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 
+@ExtendWith(MockitoExtension.class)
 class OrderTest {
 
+    @Mock
+    Clock clock;
+    private Instant now;
+
     @BeforeEach
-    void setUp() throws Exception {}
+    void setUp() {
+       this.now = Instant.now();
+    }
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void OrderExpirationTest() {
+        Order order = new Order(clock);
+        Instant endTime = now.plus( 25, ChronoUnit.HOURS);
+        when(clock.getZone()).thenReturn(ZoneId.systemDefault());
+        when(clock.instant()).thenReturn(now).thenReturn(endTime);
+
+        order.submit();
+
+        assertThrows(OrderExpiredException.class, () -> order.confirm());
     }
 
 }
